38461523fea72a06cfbaad9ce248937cb0d7c972
caharper <caharper23@gmail.com>
2024-03-04 17:33:09 -0600

    Deploying first iteration

---
+++ assets/index.html-CutByM1w.js
@@ -0,0 +1,89 @@
+import{_ as g,r as i,o as h,c as u,a as n,b as t,w as a,d as c,e as m,f as r}from"./app-Dw2r7XDh.js";const b={},_=m('<h2 id="about-me" tabindex="-1"><a class="header-anchor" href="#about-me" aria-hidden="true">#</a> About Me</h2><p>I am currently a PhD candidate in machine learning, where my focus is on exploring the intersection of of digital signal processing and neural networks. My aim is to explore ways to enhance the learning efficiency and capacity of neural networks, reducing reliance on extensive architecture design, boosting performance across various tasks, and saving computational and monetary resources. I love presenting complex concepts in a clear and engaging way, making technology more accessible to everyone. My passion lies in building AI products and systems that improve daily life by removing repetitive and mundane tasks, allowing people to focus on creativity and innovation. <a href="/about">More</a></p><h2 id="my-publication-highlights" tabindex="-1"><a class="header-anchor" href="#my-publication-highlights" aria-hidden="true">#</a> My Publication Highlights</h2>',3),f=r("h2",{id:"project-highlights",tabindex:"-1"},[r("a",{class:"header-anchor",href:"#project-highlights","aria-hidden":"true"},"#"),c(" Project Highlights")],-1),y=r("h2",{id:"blog-highlights",tabindex:"-1"},[r("a",{class:"header-anchor",href:"#blog-highlights","aria-hidden":"true"},"#"),c(" Blog Highlights")],-1);function k(v,w){const l=i("Image"),d=i("CenteredText"),o=i("SVG"),p=i("SVGCarousel"),e=i("HighlightCard"),s=i("HighlightCardCarousel");return h(),u("div",null,[n(' <RoundImage src="./../src/.vuepress/public/assets/image/clayton_round_picture.png" alt="Your Picture" class="homepage-image" /> '),t(l,{imagePath:"/assets/image/clayton_round_picture.png",altText:"Clay Harper",captionText:"Clayton Harper"}),t(d,{fontSize:"1.5"},{default:a(()=>[c("Machine Learning Specialist | PhD Candidate in Computer Science")]),_:1}),t(p,null,{default:a(()=>[t(o,{path:"/assets/image/email",alt:"Email",href:"mailto:caharper@smu.edu",size:"3rem"}),t(o,{path:"/assets/image/github",alt:"GitHub",href:"https://github.com/caharper",size:"3rem"}),t(o,{path:"/assets/image/linkedin",alt:"LinkedIn",href:"https://www.linkedin.com/in/clay-harper-6a2b94141/",size:"3rem"}),t(o,{path:"/assets/image/google_scholar",alt:"Google Scholar",href:"https://scholar.google.com/citations?user=cZxMJEEAAAAJ&hl=en",size:"3rem"})]),_:1}),n(" Use the below to change the path to the svgs (light and dark) "),n(" {{ $isDarkmode }} "),n(" icon: fas fa-github "),_,t(s,{moreLink:"/publications"},{default:a(()=>[t(e,{imageSrc:"/assets/publications/learnable_moments/learnable_moments_method_overview.png",title:"Learnable Statistical Moments",description:"Recently accepted in ICASSP 2024.",link:"/publications/learnable_moments"}),t(e,{imageSrc:"/assets/publications/amc_deep_neural_nets/mod_signals.gif",title:"AMC with Deep Neural Networks",description:"Published in MDPI Electronics, 2023.",link:"/publications/amc_deep_neural_nets"}),t(e,{imageSrc:"/assets/publications/snr_boosted/snr_comparison.png",title:"SNR-Boosted AMC",description:"Published in Asilomar, 2021.",link:"/publications/snr_boosted_amc"}),t(e,{imageSrc:"/assets/publications/synthetic_data_trackformer/trackformer_demo.gif",title:"Synthetic Data Analysis on TrackFormer",description:"Published in AIPR, 2023.",link:"/publications/synthetic_data_trackformer"})]),_:1}),f,t(s,{moreLink:"/projects"},{default:a(()=>[n(` <HighlightCard
+  imageSrc="/assets/projects/smart_writer.gif"
+  title="Smart TFRecord Writer"
+  description="A Python package to simplify writing data to TFRecord files."
+  link="/projects/smart_tfrecord_writer"
+/> `),t(e,{imageSrc:"/assets/projects/spectral_conv.png",title:"Spectral Convolution in Neural Networks",description:"Leveraging Fourier analysis to enhance neural network performance.",link:"/projects/spectral_convolution"}),t(e,{imageSrc:"/assets/projects/dct_demo.gif",title:"Learnable DCT-Resizing",description:"A differentiable method for learning intermediate feature map resolutions.",link:"/projects/dct_resizing"}),t(e,{imageSrc:"/assets/projects/smart_tfrecord_writer_overview.jpg",title:"Smart TFRecord Writer",description:"A Python package to simplify writing data to TFRecord files.",link:"/projects/smart_tfrecord_writer"}),t(e,{imageSrc:"/assets/projects/cmos_matching.gif",title:"CMOS-Based Rotational Spectroscopy",description:"Neural networks for identifying chemical compounds from rotational spectroscopy data.",link:"/projects/cmos_spectroscopy"})]),_:1}),y,t(s,{moreLink:"/blog"},{default:a(()=>[t(e,{imageSrc:"/assets/blog/dct/dct_waves.gif",title:"Discrete Cosine Transform Basics",description:"A tutorial on the basics of the discrete cosine transform.",link:"/blog/frequency_analysis/dct_basics"}),t(e,{imageSrc:"/assets/blog/gradient_checkpoint_logo.png",title:"Gradient Checkpointing",description:"Learn how to reduce memory usage in backpropagation in TensorFlow.",link:"/blog/understanding_ml/gradient_checkpointing"}),t(e,{imageSrc:"/assets/blog/receptive_field/receptive_field.gif",title:"Understanding Receptive Field",description:"What is receptive field in convolutional neural networks?",link:"/blog/understanding_ml/understanding_receptive_field"}),t(e,{imageSrc:"/assets/blog/endpoint_logo.png",title:"Understanding Endpoint Layers",description:"Up your game in designing loss functions and more sophisticated neural network architectures.",link:"/blog/understanding_ml/endpoint_layers"})]),_:1}),n(" Here, I want to add articles/projects that have cool figures to link to in image cards "),n(` ## Testing VueBootstrap
+
+<BButton text="Click me" btnStyle="primary"></BButton>
+<button type="button" class="btn btn-primary">Primary</button>
+
+### This is a bootstrap table
+
+<!-- <script setup>
+  import Vue from 'vue'
+  import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+  Vue.use(BootstrapVue)
+  Vue.use(IconsPlugin)
+<\/script> `),n(` <div>
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handled</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+    </tbody>
+  </table>
+</div> `),n(` <div>
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+  </div>
+
+<Button type="primary">Primary</Button>
+
+<!-- <script>
+  // import { BButton } from 'bootstrap-vue'
+  import { BootstrapVue, IconsPlugin } from "bootstrap-vue"
+<\/script> `),n(` ---
+
+home: true
+title: Documentation
+heroText: false
+heroImage: /images/logotype/dxos-hero.svg
+heroImageDark: /images/logotype/dxos-hero-white.svg
+actions:
+
+- text: Get started
+  link: /guide/getting-started
+  type: primary
+- text: Join Discord
+  link: https://discord.gg/eXVfryv3sW
+  type: discord
+  features:
+- title: 'ECHO Database'
+  details: Peer-to-peer data synchronization for real time and offline-first applications.
+- title: 'HALO Identity'
+  details: Private, secure, and simple decentralized identity and verifiable credentials.
+- title: 'MESH Networking'
+  details: Resilient snd secure peer-to-peer networks, peer discovery, NAT traversal.
+- title: 'KUBE Network'
+  details: Peer-to-peer infrastructure that supports the operation of the DXOS network.
+- title: 'Devtools'
+  details: Command line and browser tools to create and publish applications, and manage KUBE network infrastructure.
+- title: 'Apps and Components'
+  details: PWA project templates and React UI components.
+  footer: MIT Licensed | Copyright © DXOS.org
+
+--- `)])}const C=g(b,[["render",k],["__file","index.html.vue"]]);export{C as default};
